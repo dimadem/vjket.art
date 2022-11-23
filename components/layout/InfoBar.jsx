754b2a6d3c193a06664bc87386aa76ea173f5a78
Dima <iamdimadem@gmail.com>
2022-11-23 16:59:00 +0300

    Revert "init commit"
    This reverts commit b715d9e50fe72bf74974bdb009b696842bff2c30.

--- components/layout/InfoBar.jsx
+++
@@ -1,8 +0,0 @@
-export default function InfoBar() {
-  return (
-    // ????
-    <div className="fixed top-0 right-0 bg-white h-screen">
-      <p className="text-3xl">2022</p>
-    </div>
-  );
-}
--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,7 @@
-// import "../styles/globals.css";
-
-// function MyApp({ Component, pageProps }) {
-//   return <Component {...pageProps} />;
-// }
-
-// export default MyApp;
-
-import Head from "next/head";
-import { ThemeProvider } from "next-themes";
-import { Red_Hat_Mono } from "@next/font/google";
-import "../styles/globals.css";
-
-// add font in Tailwind style
-const redhatmono = Red_Hat_Mono({
-  subsets: ["latin"],
-  variable: "--font-redhatmono",
-});
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <style jsx global>{`
-        html {
-          font-family: ${redhatmono.style.fontFamily};
-        }
-      `}</style>
-      <Head>
-        <title>welcone to VJkET website</title>
-        <link rel="icon" href="./favicon.ico" />
-      </Head>
-      <ThemeProvider attribute="class" enableSystem={false}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
