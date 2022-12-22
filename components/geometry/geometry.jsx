080dd40d9c356b33bf303396b07b8edd7496ec34
Dima <iamdimadem@gmail.com>
2022-12-22 14:42:10 +0000

    add postProcessing

--- components/geometry/geometry.jsx
+++ components/geometry/geometry.jsx
@@ -1,12 +1,21 @@
 import { BufferGeometry, Vector3, DoubleSide } from "three";
-import { useRef, useMemo } from "react";
-import { useFrame } from "@react-three/fiber";
+import Link from "next/link";
+import { useRef, useMemo, useContext, useState } from "react";
+import { useFrame, extend } from "@react-three/fiber";
+import { Center, MeshDistortMaterial, Text3D } from "@react-three/drei";
+import { MenuContext } from "../../context/menu.context";
+// extend({ Link });
 
 export default function Poly() {
+  const { menu, setMenu } = useContext(MenuContext);
+  const { disciplines, years } = menu;
   const mesh = useRef();
+  const [hoveredPoly, hoverPoly] = useState(true);
+  const [hoveredYear, hoverYear] = useState(true);
+  const [useColor, setColor] = useState("#4B4B4B"); // color of text for dark and white mode
 
   useFrame((state, delta) => {
-    mesh.current.rotation.y += delta / 5;
+    mesh.current.rotation.y += delta / 10;
   }, []);
 
   let geometry = useMemo(() => {
@@ -15,27 +24,76 @@ export default function Poly() {
       new Vector3(-1, 1, -1), //c
       new Vector3(-1, -1, 1), //b
       new Vector3(1, 1, 1), //a
-
       new Vector3(1, 1, 1), //a
       new Vector3(1, -1, -1), //d
       new Vector3(-1, 1, -1), //c
-
       new Vector3(-1, -1, 1), //b
       new Vector3(1, -1, -1), //d
       new Vector3(1, 1, 1), //a
-
       new Vector3(-1, 1, -1), //c
       new Vector3(1, -1, -1), //d
       new Vector3(-1, -1, 1), //b
     ];
     g.setFromPoints(points);
     g.computeVertexNormals();
+    // console.log("points", points);
+
     return g;
   }, []);
 
+  const pointsForYears = [
+    [1, 1, 1], //a
+    [-1, -1, 1], //b
+    [-1, 1, -1], //c
+    [1, -1, -1], //d
+  ];
+
+  years.forEach((year, index) => {
+    // console.log("year", year);
+    // console.log("index", index);
+    // console.log("pointsForYears[index]", pointsForYears[index]);
+    year.position = pointsForYears[index];
+  });
+
+  // console.log("YEARS ADD", years);
+
   return (
-    <mesh ref={mesh} scale={2} geometry={geometry}>
-      <meshStandardMaterial color="darkGrey" side={DoubleSide} />
-    </mesh>
+    <>
+      <group ref={mesh} scale={1.5}>
+        <mesh
+          geometry={geometry}
+          position={[0, 0, 0]}
+          onPointerOver={() => hoverPoly(false)}
+          onPointerOut={() => hoverPoly(true)}
+          renderOrder="0"
+        >
+          <MeshDistortMaterial
+            distort={0.2}
+            // speed={1}
+            roughness={0}
+            color="#4B4B4B"
+            side={DoubleSide}
+            wireframe={hoveredPoly ? true : false}
+          />
+        </mesh>
+
+        {years &&
+          years.map(({ slug, title, _id, position }) => (
+            <Text3D
+              key={_id}
+              position={position} // pos of each text
+              font="../Red_Hat_Mono-Light_Regular.json"
+              size={0.15}
+              height={0.02}
+              renderOrder="1"
+              // onClick={() => (window.location.href = `/year/${slug}`)} // use next/link
+            >
+              {/* <link href={`/year/${slug}`}>{title}</link> */}
+              {title}
+              <meshBasicMaterial color="black" />
+            </Text3D>
+          ))}
+      </group>
+    </>
   );
 }
--- components/layout/PolyMenu.jsx
+++ components/layout/PolyMenu.jsx
@@ -2,28 +2,47 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import Poly from "../geometry/geometry";
+import {
+  EffectComposer,
+  DepthOfField,
+  Bloom,
+  Noise,
+  Vignette,
+  Glitch,
+} from "@react-three/postprocessing";
+import { GlitchMode } from "postprocessing";
 
 export default function PolyMenu() {
   return (
     <div className="flex order-2 bg-neutralWhite dark:bg-black w-screen h-screen">
-      {/* <video
-        className="w-screen h-screen object-cover"
-        muted
-        loop
-        autoPlay
-        src="./media/polymenu/polymenu2.mp4"
-        type="video/mp4"
-      /> */}
-
-      <Canvas className="w-screen h-screen">
+      <Canvas className="w-screen h-screen" gl={{ antialias: false }}>
+        {/* background */}
+        {/* set color for darkmode and lightmode */}
+        <color args={["#D9D9D9"]} attach="background" />
+        {/* post processing */}
+        <EffectComposer>
+          <DepthOfField
+            focusDistance={0}
+            focalLength={0.02}
+            bokehScale={2}
+            height={720}
+          />
+          <Noise opacity={0.1} />
+          <Glitch
+            delay={[0.5, 2]}
+            duration={[0.01, 0.2]}
+            // strength={[0.2, 0.4]}
+            mode={GlitchMode.SPORADIC}
+          />
+          <Vignette offset={0.1} darkness={0.6} />
+        </EffectComposer>
         {/* performance data */}
         {/* <Perf position="bottom-left" /> */}
         {/* Camera */}
         <OrbitControls makeDefault />
         {/* lights */}
         <ambientLight intensity={0.5} />
-        <directionalLight position={[1, 2, 3]} intensity={1.5} />
-
+        <directionalLight position={[-10, 10, 5]} intensity={1.5} />
         {/* Poly */}
         <Poly />
       </Canvas>
