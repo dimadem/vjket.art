30ffe944f3065ea7910bf4fe666ad69196fcb14b
Dima <iamdimadem@gmail.com>
2023-01-09 14:02:54 +0000

    fix code

--- components/layout/PolyMenu.jsx
+++ components/layout/PolyMenu.jsx
@@ -4,12 +4,10 @@ import { Perf } from "r3f-perf";
 import Poly from "../geometry/geometry";
 import {
   EffectComposer,
-  DepthOfField,
   Noise,
   Vignette,
   Glitch,
 } from "@react-three/postprocessing";
-// import { GlitchMode } from "postprocessing";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 export default function PolyMenu() {
@@ -22,11 +20,6 @@ export default function PolyMenu() {
     if (theme == "dark") setColor("#4B4B4B");
     if (theme == "light") setColor("#D9D9D9");
   }, [theme]);
-
-  const black = "#171717";
-  const white = "#f1f1f1";
-  const neutralBlack = "#4B4B4B";
-  const neutralWhite = "#D9D9D9";
 
   return (
     <div className="flex order-2 bg-neutralWhite dark:bg-black w-screen h-screen">
@@ -46,6 +39,7 @@ export default function PolyMenu() {
           enableRotate={true}
           enableZoom={false}
         />
+
         {/* lights */}
         <ambientLight intensity={0.5} />
         <directionalLight position={[-10, 10, 5]} intensity={1.5} />
@@ -55,15 +49,8 @@ export default function PolyMenu() {
 
         {/* Effects */}
         <EffectComposer>
-          {/* <DepthOfField
-            focusDistance={0.0025}
-            focalLength={0.007}
-            bokehScale={4}
-            height={240}
-          /> */}
-          <Noise opacity={0.7} />
+          <Noise opacity={0.6} />
           <Glitch delay={[0.5, 2]} duration={[0.01, 0.2]} />
-
           <Vignette offset={0.1} darkness={0.6} />
         </EffectComposer>
       </Canvas>
